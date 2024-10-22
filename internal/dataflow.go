9e1b5c9b1e1204c4d9cf80df3a0c192bda0376b5
simon-desktop <330771794@qq.com>
2024-10-22 18:25:05 +0800

    [Test]test

--- internal/dataflow.go
+++ internal/dataflow.go
@@ -11,18 +11,20 @@ import (
 func writer(stream io.Writer, out io.Reader, ctx context.Context) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
-	buffer := make([]byte, 4*1024)
+	//buffer := make([]byte, 4*1024)
 	for common.Opened(ctx) {
-		_, _ = io.CopyBuffer(stream, out, buffer)
+		//_, _ = io.CopyBuffer(stream, out, buffer)
+		_, _ = io.Copy(stream, out)
 	}
 }
 
 func reader(stream io.Reader, in io.Writer, ctx context.Context) {
 	runtime.LockOSThread()
 	defer runtime.UnlockOSThread()
-	buffer := make([]byte, 4*1024)
+	//buffer := make([]byte, 4*1024)
 	for common.Opened(ctx) {
-		_, err := io.CopyBuffer(in, stream, buffer)
+		_, err := io.Copy(in, stream)
+		//_, err := io.CopyBuffer(in, stream, buffer)
 		if err != nil {
 			log.Println(err)
 			return
