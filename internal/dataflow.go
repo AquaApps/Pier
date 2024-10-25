4973540439938d11cede8167282a5f1a46873481
simon-desktop <330771794@qq.com>
2024-10-25 10:32:55 +0800

    [Benchmark]

--- internal/dataflow.go
+++ internal/dataflow.go
@@ -4,49 +4,21 @@ import (
 	"Pier/common"
 	"context"
 	"io"
-	"log"
-	"runtime"
 )
 
-func writer(stream io.Writer, out io.Reader, ctx context.Context) {
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-	buffer := make([]byte, 4*1024)
+func writer(stream io.Writer, out <-chan []byte, ctx context.Context) {
 	for common.Opened(ctx) {
-		num, _ := out.Read(buffer)
-		_, _ = stream.Write(buffer[:num])
+		_, _ = stream.Write(<-out)
 	}
 }
 
-func reader(stream io.Reader, in io.Writer, ctx context.Context) {
-	runtime.LockOSThread()
-	defer runtime.UnlockOSThread()
-	buffer := make([]byte, 4*1024)
+func reader(stream io.Reader, in chan<- []byte, ctx context.Context) {
+	packet := make([]byte, 4*1024)
 	for common.Opened(ctx) {
-		_, _ = stream.Read(buffer)
-
-		_, err := in.Write(buffer)
+		num, err := stream.Read(packet)
 		if err != nil {
-			log.Println("read", err)
 			return
 		}
-		//n, _ := device._f.Write(fun(<-device._inputStream))
-		//device.incrReadBytes(n)
-		//n, err := stream.Read(buffer)
-		//if err != nil {
-		//	log.Println("read", err)
-		//	return
-		//}
-		//_, err = in.Write(buffer[:n])
-		//if err != nil {
-		//	log.Println("write", err)
-		//	return
-		//}
-		////_, err := io.Copy(in, stream)
-		//////_, err := io.CopyBuffer(in, stream, buffer)
-		//if err != nil {
-		//	log.Println(err)
-		//	return
-		//}
+		in <- packet[:num]
 	}
 }
